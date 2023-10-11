823beb4f65af439f3ff2a3ada0a4a6da684e3daf
dennis <denhouston1@icloud.com>
2023-10-11 15:52:41 +0100

    Adding context to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
 import { Contact } from "./pages/contact";
 import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
     <div className="App">
+      <ShopContextProvider>
         <Router>
           <Navbar />
           <Routes>
@@ -17,6 +19,7 @@ function App() {
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </Router>
+      </ShopContextProvider>
     </div>
   );
 }
