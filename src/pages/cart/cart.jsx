2a3435899ae3def72a0967ae2c491dba4c2b49e1
dennis <denhouston1@icloud.com>
2023-10-12 17:01:26 +0100

    Small refactor:

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,8 +3,8 @@ import { ShopContext } from "../../context/shop-context";
 import bands from "../../products/bandData.json";
 import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
-
 import "./cart.css";
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
